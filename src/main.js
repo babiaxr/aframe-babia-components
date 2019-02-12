c418ac9dbba4a35bde689e57596aa09f2885b353
David Moreno <dmorenolumb@gmail.com>
2019-02-12 17:42:13 +0100

    Add keyboard and search data from username

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import './querier_github'
 import './vismapper'
 import './visdata'
 import 'aframe-layout-component'
+import 'aframe-super-keyboard'
+import 'aframe-text-geometry-component'
+
 
 
 
--- src/painter.js
+++ src/painter.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-    let colors= ["#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
-        "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
-        "#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
-        "#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,
-        "#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,
-        "#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,
-        "#935b6d" ,"#916988" ,"#513d98" ,"#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
+    let colors = ["#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177", "#0d5ac1",
+        "#f205e6", "#1c0365", "#14a9ad", "#4ca2f9", "#a4e43f", "#d298e2", "#6119d0",
+        "#d2737d", "#c0a43c", "#f2510e", "#651be6", "#79806e", "#61da5e", "#cd2f00",
+        "#9348af", "#01ac53", "#c5a4fb", "#996635", "#b11573", "#4bb473", "#75d89e",
+        "#2f3f94", "#2f7b99", "#da967d", "#34891f", "#b0d87b", "#ca4751", "#7e50a8",
+        "#c4d647", "#e0eeb8", "#11dec1", "#289812", "#566ca0", "#ffdbe1", "#2f1179",
+        "#935b6d", "#916988", "#513d98", "#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
         "#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
         "#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
         "#5be4f0", "#57c4d8", "#a4d17a", "#225b8", "#be608b", "#96b00c", "#088baf",
@@ -40,19 +40,40 @@ window.onload = () => {
         "#f812b3", "#b17fc9", "#8d6c2f", "#d3277a", "#2ca1ae", "#9685eb", "#8a96c6",
         "#dba2e6", "#76fc1b", "#608fa4", "#20f6ba", "#07d7f6", "#dce77a", "#77ecca"]
 
-    document.getElementById("queriertest").addEventListener('dataReadyqueriertest', function (e) {
-        let dataRetrieved = e.detail;
-        console.log("AQUI ESTA", dataRetrieved)
-        let max = Math.max.apply(Math, dataRetrieved.map(function(o) { return o.open_issues; }))
+
+    document.getElementById("keyboard").addEventListener('superkeyboardinput', function (f) {
+        // Add querier when insert username
+        let username = f.target.components['super-keyboard'].data.value
+        let querier = document.createElement('a-entity');
+        querier.setAttribute('querier_github', 'user: ' + username);
+        querier.setAttribute('id', 'queriertest');
+        document.getElementById("AframeScene").appendChild(querier)
+
+        //Check if data is retrieved and draw city
+        let querierElement = document.getElementById('queriertest')
+        if (querierElement.getAttribute('data_retrieved')) {
+            let dataRetrieved = JSON.parse(querierElement.getAttribute('data_retrieved'))
+            generateCity(dataRetrieved)
+        } else {
+            document.getElementById("queriertest").addEventListener('dataReadyqueriertest', function (e) {
+                let dataRetrieved = e.detail;
+                console.log("AQUI ESTA", dataRetrieved)
+                generateCity(dataRetrieved)
+            });
+        }
+
+    });
+
+    function generateCity(dataRetrieved) {
+        let max = Math.max.apply(Math, dataRetrieved.map(function (o) { return o.open_issues; }))
         let city = document.createElement('a-entity');
-        city.setAttribute('layout', 'type: box; columns: ' + dataRetrieved.length / 4 + '; margin: ' + (5 + max/2) + '; plane: xz; align: center');
+        city.setAttribute('layout', 'type: box; columns: ' + dataRetrieved.length / 4 + '; margin: ' + (5 + max / 2) + '; plane: xz; align: center');
         for (let index = 0; index <= dataRetrieved.length; index++) {
             let box = generateBox(index);
             city.appendChild(box)
         };
         document.getElementById("AframeScene").appendChild(city)
-
-    });
+    }
 
     function generateBox(index) {
         let entity = document.createElement('a-entity');
