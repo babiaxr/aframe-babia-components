99649d9945afa3d985e175eae2e88008c65d6585
David Moreno <dmoreno@bitergia.com>
2020-04-25 02:05:06 +0200

    Show empty fence when a building doesn't exist in the present
    This feature of the time evolution will add empty buildings children
    to a quarter when its data (the file in the examples) doesn't exist
    in the present but used to exist in the past. This allow the
    application to make a time evolution backwards, it means that
    the empty zones showed in the present will be filled in the past
    time snapshots.
    
    The dataframes has now more enriched information in order to have
    this feature, the backups have been updated.
    
    The examples of Perceval and Angular have been updated.
    
    Signed-off-by: David Moreno <dmoreno@bitergia.com>

--- tools/generate_structure_codecityjs.py
+++ tools/generate_structure_codecityjs.py
@@ -40,11 +40,11 @@ from elasticsearch.connection import create_ssl_context
 
 
 HTTPS_CHECK_CERT = False
-INDEX_DATA_FILE = 'index_backups/index_backup_graal_cocom_incubator_bootstrap.json'
-DATAFRAME_CSV_EXPORT_FILE = 'df_backups/index_dataframe_graal_cocom_incubator_bootstrap.csv'
-DATAFRAME_CSV_ENRICHED_EXPORT_FILE = 'df_backups/index_dataframe_graal_cocom_incubator_enriched_bootstrap.csv'
+INDEX_DATA_FILE = 'index_backups/index_backup_graal_cocom_incubator.json'
+DATAFRAME_CSV_EXPORT_FILE = 'df_backups/index_dataframe_graal_cocom_incubator.csv'
+DATAFRAME_CSV_ENRICHED_EXPORT_FILE = 'df_backups/index_dataframe_graal_cocom_incubator_enriched.csv'
 
-CODECITY_OUTPUT_DATA = '../examples/codecityjs/time_evolution_bootstrap/'
+CODECITY_OUTPUT_DATA = '../examples/codecityjs/time_evolution/'
 
 HEIGHT_FIELD = 'loc'
 AREA_FIELD = 'num_funs'
@@ -173,6 +173,7 @@ def generate_entities(data, entities):
                 new_leaf = {
                     "id": leaf['id'],
                     "area": leaf['value'],
+                    "max_area": leaf['value_max'],
                     "height": leaf['height']
                 }
                 new_item['children'].append(new_leaf)
@@ -291,8 +292,11 @@ def enrich_data(df, repo):
         tree_folder = row['file_path'].split(os.sep)
         df.set_value(i, 'file_path_list', str(tree_folder))
         df.set_value(i, 'n_folders', len(tree_folder))
+        acc = ""
         for n, item in enumerate(tree_folder):
+            acc += "/" + item
             df.set_value(i, 'folder_{}'.format(n), item)
+            df.set_value(i, 'folder_acc_{}'.format(n), acc)
 
     # Normalize height
     df = normalize_column(df, HEIGHT_FIELD, 0.1, 20)
@@ -330,7 +334,7 @@ def extract_data(df_raw, date):
                 #"value": df_repo['loc'].sum(),
                 "value": len(df_repo.index)
             }
-            build_folders(df_repo, entity_repo['children'], 0, df['n_folders'].max())
+            build_folders(df_repo, entity_repo['children'], 0, df['n_folders'].max(), df_raw)
 
             entity_project['children'].append(entity_repo)
 
@@ -353,29 +357,66 @@ def filter_closest_date(df, date):
     return df_filtered
 
 
-def build_folders(df, arr, index, max_levels):
+def build_folders(df, arr, index, max_levels, df_raw):
     # leafs folder
     leafs_folder = {"id": ".", 'children': []}
 
-    for folder in df['folder_{}'.format(index)].unique():
+    for acc_folder in df['folder_acc_{}'.format(index)].unique():
+        if str(acc_folder) != 'nan':
+            folder = acc_folder.split("/")[-1]
+        else:
+            folder = acc_folder
         df_folder = df[df['folder_{}'.format(index)] == folder]
         if str(folder) != 'nan':
             # Is leaf or not in order to put height
-            if (index == max_levels-1) or (len(df_folder['folder_{}'.format(index + 1)].unique()) == 1 and str(df_folder['folder_{}'.format(index + 1)].unique()[0]) == 'nan'):
+            if (index == max_levels-1) or \
+                    (len(df_folder['folder_{}'.format(index + 1)].unique()) == 1 and
+                     str(df_folder['folder_{}'.format(index + 1)].unique()[0]) == 'nan'):
+                df_raw_filtered = df_raw[df_raw['folder_acc_{}'.format(index)] == acc_folder]
                 leaf = {
                     "id": df_folder['file_path'].values[0],
                     "name": str(folder),
                     'height': max(df_folder['{}_normalized'.format(HEIGHT_FIELD)].sum(), 0.1),
-                    'value': df_folder['{}_normalized'.format(AREA_FIELD)].sum()
+                    'value': df_folder['{}_normalized'.format(AREA_FIELD)].sum(),
+                    'value_max': df_raw_filtered['{}_normalized'.format(AREA_FIELD)].max()
                 }
                 leafs_folder['children'].append(leaf)
+            # Is parent of leaf
+            elif (index == max_levels-2) or \
+                    (len(df_folder['folder_{}'.format(index + 2)].unique()) == 1 and
+                     str(df_folder['folder_{}'.format(index + 2)].unique()[0]) == 'nan'):
+                df_raw_filtered = df_raw[df_raw['folder_acc_{}'.format(index)] == acc_folder]
+                entity_folder = {
+                    "id": str(folder),
+                    'children': [],
+                    'value': len(df_raw_filtered['file_path'].unique())
+                }
+                build_folders(df_folder, entity_folder['children'], index + 1, max_levels, df_raw)
+                
+                # Add with height negative the files that will exist in the past but not exist in the present
+                for j, row in df_raw_filtered.iterrows():
+                    if not any(x["id"] == row['file_path'] for x in entity_folder['children'][0]['children']):
+                        df_raw_filtered_leaf = df_raw[df_raw['folder_acc_{}'.format(index+1)]
+                                                      == row['folder_acc_{}'.format(index+1)]]
+                        leaf_not_now = {
+                            "id": row['file_path'],
+                            "name": str(row['folder_{}'.format(index+1)]),
+                            'height': -0.2,
+                            'value': row['{}_normalized'.format(AREA_FIELD)],
+                            'value_max': df_raw_filtered_leaf['{}_normalized'.format(AREA_FIELD)].max()
+                        }
+                        entity_folder['children'][0]['children'].append(leaf_not_now)
+                ###################################################################
+                
+                arr.append(entity_folder)
             else:
+                # df_raw_filtered = df_raw[df_raw['folder_{}'.format(index)] == folder]
                 entity_folder = {
                     "id": str(folder),
                     'children': [],
                     'value': len(df_folder.index)
                 }
-                build_folders(df_folder, entity_folder['children'], index + 1, max_levels)
+                build_folders(df_folder, entity_folder['children'], index + 1, max_levels, df_raw)
                 arr.append(entity_folder)
 
     # Adds if filled
