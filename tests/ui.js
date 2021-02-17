9a6575c31799b56a90c13362a46c607a190d325c
Andrea Villaverde <camaratomoyo@hotmail.com>
2021-02-17 21:07:18 +0100

    Add demo UI component for Oculus Quest

--- tests/ui.js
+++ tests/ui.js
@@ -4,7 +4,7 @@
 
 describe('BabiaXR-Simplebarchart component examples (screenshot)', () => {
 
-    ['3dcylinder', 'pie', 'simplebar', 'city_treegenerator', 'island_treegenerator', 'ui_treegenerator', '3dbars'].forEach((example) => {
+    ['3dcylinder', 'pie', 'simplebar', 'city_treegenerator', 'island_treegenerator', 'ui_treegenerator', '3dbars', 'oculus'].forEach((example) => {
         it(`Screenshot Simplebarchart (${example})`, () => {
             cy.visit('/examples/ui/'+ example + '/index.html');
             cy.wait(5000);
