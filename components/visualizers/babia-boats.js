3f777e478586437f9b572d14a7f8f7e2e2d51891
David Moreno <dmoreno@bitergia.com>
2021-04-06 19:56:44 +0200

    Update color gradient on boats from raimbow to blue to red
    Signed-off-by: David Moreno <dmoreno@bitergia.com>

--- components/visualizers/babia-boats.js
+++ components/visualizers/babia-boats.js
@@ -1154,8 +1154,20 @@ let getMaxMinColorValues = (data, colorfield, max, min) => {
 }
 
 function heatMapColorforValue(val, max, min) {
+
+    //Blue to red
+    let color2 = [19, 82, 138]
+    let color1 = [255, 94, 83]
+
     let value = ((val || 0) - min) / (max - min)
 
-    let h = (1.0 - value) * 240
-    return "hsl(" + h + ", 100%, 50%)";
+    var p = value;
+    var w = p * 2 - 1;
+    var w1 = (w / 1 + 1) / 2;
+    var w2 = 1 - w1;
+    var rgb = [Math.round(color1[0] * w1 + color2[0] * w2),
+    Math.round(color1[1] * w1 + color2[1] * w2),
+    Math.round(color1[2] * w1 + color2[2] * w2)];
+    return "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+
 }
