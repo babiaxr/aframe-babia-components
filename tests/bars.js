8b3190ff5448a262e7e79c5293adb3d36951a8ed
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2021-05-15 19:59:48 +0200

    [bars] Fix problem when x_axis is not a valid id
    In HTML, id attributes cannot include spaces. Besides,
    they should be unique in the whole document. Currently,
    babia-bars is using ids to track bars, and those ids
    are build from x_axis values. Therefore, ids may have
    spaces if x_axis have them, and they are not unique if
    the same x_axis value is used in two different charts.
    
    This patch fixes both problems by using a new attribute,
    babia-name, instead if id, to track bars.
    
    A test is included.

--- tests/bars.js
+++ tests/bars.js
@@ -9,10 +9,14 @@ describe ('Babia-Bars component', () => {
       });
 
     it ('Simple Creation', () => {
-        let data = "[{'key':'kbn_network','size':10},{'key':'Maria','size':5},{'key':'Dave','size':90},{'key':'Jhon','size':119},{'key':'Sara','size':16},{'key':'Lemar','size':2},{'key':'Dawn','size':1},{'key':'Jesus','size':8},{'key':'Bitergia','size':3},{'key':'URJC','size':6},{'key':'Alice','size':22},{'key':'Pete','size':2},{'key':'Seth','size':6},{'key':'Martin','size':9}]";
+        let data = `[{"key":"Network","size":10},{"key":"Door","size":5}],
+            {"key":"Table","size":90},{"key":"Node","size":119},{"key":"Cable","size":16},
+            {"key":"Cloud","size":2},{"key":"Sky","size":1},{"key":"Portrait","size":8},
+            {"key":"Chair","size":3},{"key":"Computer","size":6},{"key":"Alias","size":22},
+            {"key":"Armchair","size":2},{"key":"Radio","size":6},{"key":"Kangaroo","size":9}]`;
         cy.get('a-scene').then(scene => {
             // Add components     
-            let chart = Cypress.$('<a-entity babia-bars="legend: true; axis: true; data: '+ data + '" ></a-entity>');
+            let chart = Cypress.$("<a-entity babia-bars='legend: true; axis: true; data: "+ data + "' ></a-entity>");
             Cypress.$(scene).append(chart); //appendchild
         });
         
@@ -44,6 +48,33 @@ describe ('Babia-Bars component', () => {
             .should('nested.include', {'x_axis': 'name'})
             .should('nested.include', {'height': 'size'});
     }); 
+
+    /*
+     * Id cannot have spaces, so an implementation that converts keys
+       to spaces doesn't work. When we fixed this, we added this test
+       to avoid regression.
+     */
+    it ('Creation with keys which cannot be ids', () => {
+        let data = '[{"x_axis":"Name with spaces","height":10},{"x_axis":"Maria","height":5}]';
+        let bars = `<a-entity babia-bars='x_axis: key; data: ${data}'></a-entity>`
+        console.log("Bars: ", bars)
+        // Add components
+            cy.get('a-scene').then(scene => {
+            let chart = Cypress.$(bars);
+            Cypress.$(scene).append(chart); //appendchild
+        });
+        
+        // Test entities existence
+        assert.exists(cy.get('a-entity[babia-bars]'));
+        // Check bars created
+        cy.get('[babia-bar]').should('have.length', 2);
+        // Change data for babia-bars
+        cy.get('[babia-bars]')
+          .invoke('attr', 'babia-bars', 'data: [{"x_axis":"Name with spaces","height":7}]')
+          // Check bars created
+        cy.get('[babia-bar]').should('have.length', 2);
+    });
+
 });
 
 describe('Babia-bars component examples (screenshot)', () => {
