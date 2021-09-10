dc2c0d4d4fcae70d580543654a9910935b675f4b
David Moreno <dmoreno@bitergia.com>
2021-09-10 13:31:30 +0200

    Move build process to webpack 4
    Signed-off-by: David Moreno <dmoreno@bitergia.com>

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "./index.js"),
+  output: {
+    filename: "aframe-babia-components.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        include: [path.resolve(__dirname, "components")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  }
+};
